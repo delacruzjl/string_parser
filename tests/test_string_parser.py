386a5a3e7e608d9e1b92b97a049270e483e435d3
Jose DelaCruz <delacruzjl@outlook.com>
2020-08-30 17:56:43 -0400

    adds unit test for float use cases

--- tests/test_string_parser.py
+++ tests/test_string_parser.py
@@ -21,11 +21,27 @@ class TestStringParser(TestCase):
         # assert
         self.assertEqual(actual, None)
 
-    def test_string_parser_valid_should_Pass(self):
+    def test_string_parser_float_percent_should_match(self):
         # arrange
-        data = 'FILESYSTEM OK -/ used space (9.5%) inode (3%) | fs=9% inode=3%'
+        data = 'FILESYSTEM OK -/ used space (9.5%) inode (3.123%) | fs=9% inode=3%'
         expected = {
             'space': '9.5%',
+            'inode': '3.123%',
+            'fs': '9%'
+        }
+
+        # act
+        actual = self.sut(data)
+
+        # assert
+        self.assertEqual(actual, expected)
+
+
+    def test_string_parser_valid_should_Pass(self):
+        # arrange
+        data = 'FILESYSTEM OK -/ used space (9%) inode (3%) | fs=9% inode=3%'
+        expected = {
+            'space': '9%',
             'inode': '3%',
             'fs': '9%'
         }
